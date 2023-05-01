77b02de071886c91ef7d71482657def3ae19ca1e
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-05-01 21:10:47 +0300

    Add 404 page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage: React.FC<any> = () => {
+  return (
+    <div>
+      <h2>Упс... Похоже вы забрели не туда :)</h2>
+    </div>
+  );
+};
+
+export default NotFoundPage;
